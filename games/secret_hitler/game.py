f88916065c83105e9a138d2f0fce14ae9c024730
Joseph Wasden <jpwasden93@gmail.com>
2024-04-11 21:46:21 +0100

    Type hint updates

--- games/secret_hitler/game.py
+++ games/secret_hitler/game.py
@@ -4,13 +4,13 @@ from games.interface.game import GameInterface
 
 
 class Game(GameInterface):
-    def __init__(self, players: set[User | Member], interaction: Interaction):
+    def __init__(self, players: dict[str, User | Member], interaction: Interaction):
         self._players = players
         self._name = "Secret Hitler"
         self.interaction = interaction
 
     @property
-    def players(self) -> set[User | Member]:
+    def players(self) -> dict[str, User | Member]:
         return self._players
 
     @property
@@ -18,7 +18,7 @@ class Game(GameInterface):
         return self._name
 
     @classmethod
-    async def setup_game(cls, interaction: Interaction, players: set[User | Member]):
+    async def setup_game(cls, interaction: Interaction, players: dict[str, User | Member]):
         return cls(players, interaction)
 
     async def run(self):
